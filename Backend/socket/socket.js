587f7eef0426d0b3e5684b9bd7698671803778ac
Yomesh Nagar <yomeshnagar2006@gmail.com>
2026-07-08 22:03:47 +0530

    Fix production cors

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -7,7 +7,7 @@ const userSocketMap = {};
 const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://convey-lime.vercel.app",
       credentials: true,
     },
   });
