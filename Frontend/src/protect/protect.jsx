9ceee41dc779ae297fdf1f33ef19f8a7aada041f
Yomesh Nagar <yomeshnagar2006@gmail.com>
2026-07-09 11:04:09 +0530

    COnvey

--- Frontend/src/protect/protect.jsx
+++ Frontend/src/protect/protect.jsx
@@ -20,7 +20,26 @@ const Protect = ({ children }) => {
     checkAuth();
   }, []);
 
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return (
+    <div className="animate-pulse">
+      {/* Search */}
+      <div className="h-11 rounded-lg bg-gray-300 mb-5"></div>
+
+      {/* Users */}
+      {[...Array(8)].map((_, i) => (
+        <div key={i} className="flex items-center gap-3 mb-5">
+          {/* Profile */}
+          <div className="w-14 h-14 rounded-full bg-gray-300"></div>
+
+          {/* Name & Message */}
+          <div className="flex-1">
+            <div className="h-4 w-40 rounded-full bg-gray-300 mb-2"></div>
+            <div className="h-3 w-24 rounded-full bg-gray-300"></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 
   if (!isAuth) {
     return <Navigate to="/login" replace />;
