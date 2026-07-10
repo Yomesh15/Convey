a3e9c1c98fd2228c36a74eb15f8c791069b3737d
Yomesh Nagar <yomeshnagar2006@gmail.com>
2026-07-10 10:15:36 +0530

    Convey

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -1,15 +1,20 @@
 import "dotenv/config";
 import nodemailer from "nodemailer";
 
-
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+
+  connectionTimeout: 60000,
+  greetingTimeout: 60000,
+  socketTimeout: 60000,
 });
 
 export default transporter;
